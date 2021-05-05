937fd93cab02ab513a1692cf10b29e4dc7668027
M3r1al <83663900+M3r1al@users.noreply.github.com>
2021-05-05 14:19:32 +0300

    Update lagrange.cs

--- lagrange.cs
+++ lagrange.cs
@@ -3,7 +3,7 @@ using Point;
 
 public class lagrange 
 {
-	Point curveLagrange(Point points, float x)
+	Point curve(Point points, float x)
 	{    
 		Point curve = new Point(x, 0);
 		for(int i = 0; i < points.Length; i++)
